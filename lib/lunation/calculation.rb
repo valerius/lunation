9088eb195218205756937055ab1dce66081bec87
Ivo Kalverboer <ivokalver@gmail.com>
2024-05-10 22:50:08 +0200

    Create moon_illuminated_fraction method

--- lib/lunation/calculation.rb
+++ lib/lunation/calculation.rb
@@ -1,4 +1,9 @@
 module Lunation
   class Calculation
+    # (k) illuminated fraction of the moon (48.1, A.A. p. 345)
+    def moon_illuminated_fraction
+      result = (1 + Math.cos(moon_phase_angle * Math::PI / 180)) / 2.0
+      result.round(4)
+    end
   end
 end
--- spec/lunation/calculation_spec.rb
+++ spec/lunation/calculation_spec.rb
@@ -1,3 +1,17 @@
 RSpec.describe Lunation::Calculation do
   let(:calculation) { described_class.new }
+
+  describe "moon_illuminated_fraction" do
+    subject(:moon_illuminated_fraction) { calculation.moon_illuminated_fraction }
+
+    before do
+      allow(calculation).to receive(:moon_phase_angle).and_return(moon_phase_angle)
+    end
+
+    let(:moon_phase_angle) { 69.0756 } # example 48.a (A.A. p. 347)
+
+    it "returns the correct value" do
+      expect(moon_illuminated_fraction).to eq(0.6786)
+    end
+  end
 end

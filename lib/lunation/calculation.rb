5cc7a2858ac6304b868b788bf3524b0a0473c250
Ivo Kalverboer <ivokalver@gmail.com>
2024-05-10 23:27:37 +0200

    Create moon_phase_angle method

--- lib/lunation/calculation.rb
+++ lib/lunation/calculation.rb
@@ -5,5 +5,13 @@ module Lunation
       result = (1 + Math.cos(moon_phase_angle * Math::PI / 180)) / 2.0
       result.round(4)
     end
+
+    # (i) phase angle of the moon (48.3, A.A. p. 346)
+    def moon_phase_angle
+      numerator = earth_sun_distance_in_km * Math.sin(moon_geocentric_elongation * Math::PI / 180)
+      denominator = earth_moon_distance - earth_sun_distance_in_km * Math.cos(moon_geocentric_elongation * Math::PI / 180)
+      result = Math.atan2(numerator, denominator) / Math::PI * 180
+      result.round(6)
+    end
   end
 end
--- spec/lunation/calculation_spec.rb
+++ spec/lunation/calculation_spec.rb
@@ -14,4 +14,24 @@ RSpec.describe Lunation::Calculation do
       expect(moon_illuminated_fraction).to eq(0.6786)
     end
   end
+
+  describe "moon_phase_angle" do
+    subject(:moon_phase_angle) { calculation.moon_phase_angle }
+
+    before do
+      allow(calculation).to receive_messages(
+        earth_sun_distance_in_km: earth_sun_distance_in_km,
+        earth_moon_distance: earth_moon_distance,
+        moon_geocentric_elongation: moon_geocentric_elongation
+      )
+    end
+
+    let(:earth_sun_distance_in_km) { 149_971_520 } # R
+    let(:earth_moon_distance) { 368_410 } # Delta
+    let(:moon_geocentric_elongation) { 110.7929 } # Psi
+
+    it "returns the correct value" do
+      expect(moon_phase_angle.round(4)).to eq(69.0756) # example 48.a (A.A. p. 347)
+    end
+  end
 end

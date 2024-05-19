0b4969bda79882a538e7d4cde34a425483c3a8a4
Ivo Kalverboer <ivokalver@gmail.com>
2024-05-19 17:57:06 +0200

    Create delta_t function

--- lib/lunation/calculation.rb
+++ lib/lunation/calculation.rb
@@ -8,6 +8,7 @@ module Lunation
 
     def initialize(datetime)
       @datetime = datetime
+      @decimal_year = datetime.year + (datetime.month - 0.5) / 12.0
     end
 
     # (k) illuminated fraction of the moon (48.1, A.A. p. 345)
@@ -50,6 +51,103 @@ module Lunation
     # (T) time, measured in Julian centuries from the Epoch J2000.0 (22.1, A.A. p. 143)
     def time
       @time ||= ((julian_ephemeris_day - 2_451_545) / 36_525.0).round(12)
+    end
+
+    # ΔT https://eclipse.gsfc.nasa.gov/SEcat5/deltatpoly.html
+    def delta_t
+      case datetime.year
+      when -1_999...-500
+        elapsed_years = (@decimal_year - 1_820) / 100
+        -20 + 32 * elapsed_years**2
+      when -500...500
+        elapsed_years = @decimal_year / 100.0
+        10_583.6 -
+          1_014.41 * elapsed_years +
+          33.78311 * elapsed_years**2 -
+          5.952053 * elapsed_years**3 -
+          0.1798452 * elapsed_years**4 +
+          0.022174192 * elapsed_years**5 +
+          0.0090316521 * elapsed_years**6
+      when 500...1_600
+        elapsed_years = (@decimal_year - 1_000.0) / 100.0
+        1_574.2 -
+          556.01 * elapsed_years +
+          71.23472 * elapsed_years**2 +
+          0.319781 * elapsed_years**3 -
+          0.8503463 * elapsed_years**4 -
+          0.005050998 * elapsed_years**5 +
+          0.0083572073 * elapsed_years**6
+      when 1_600...1_700
+        elapsed_years = @decimal_year - 1_600.0
+        120 - 0.9808 * elapsed_years -
+          0.01532 * elapsed_years**2 +
+          elapsed_years**3 / 7_129
+      when 1_700...1_800
+        elapsed_years = @decimal_year - 1_700.0
+        8.83 +
+          0.1603 * elapsed_years -
+          0.0059285 * elapsed_years**2 +
+          0.00013336 * elapsed_years**3 -
+          elapsed_years**4 / 1_174_000
+      when 1_800...1_860
+        elapsed_years = @decimal_year - 1_800
+        13.72 -
+          0.332447 * elapsed_years +
+          0.0068612 * elapsed_years**2 +
+          0.0041116 * elapsed_years**3 -
+          0.00037436 * elapsed_years**4 +
+          0.0000121272 * elapsed_years**5 -
+          0.0000001699 * elapsed_years**6 +
+          0.000000000875 * elapsed_years**7
+      when 1_860...1_900
+        elapsed_years = @decimal_year - 1_860
+        7.62 +
+          0.5737 * elapsed_years -
+          0.251754 * elapsed_years**2 +
+          0.01680668 * elapsed_years**3 -
+          0.0004473624 * elapsed_years**4 +
+          elapsed_years**5 / 233_174
+      when 1_900...1_920
+        elapsed_years = @decimal_year - 1_900
+        -2.79 +
+          1.494119 * elapsed_years -
+          0.0598939 * elapsed_years**2 +
+          0.0061966 * elapsed_years**3 -
+          0.000197 * elapsed_years**4
+      when 1_920...1_941
+        elapsed_years = @decimal_year - 1_920
+        21.20 +
+          0.84493 * elapsed_years -
+          0.076100 * elapsed_years**2 +
+          0.0020936 * elapsed_years**3
+      when 1_941...1_961
+        elapsed_years = @decimal_year - 1_950
+        29.07 +
+          0.407 * elapsed_years -
+          elapsed_years**2 / 233 +
+          elapsed_years**3 / 2_547
+      when 1_961...1_986
+        elapsed_years = @decimal_year - 1_975
+        45.45 +
+          1.067 * elapsed_years -
+          elapsed_years**2 / 260 -
+          elapsed_years**3 / 718
+      when 1_986...2_005
+        elapsed_years = @decimal_year - 2_000
+        63.86 +
+          0.3345 * elapsed_years -
+          0.060374 * elapsed_years**2 +
+          0.0017275 * elapsed_years**3 +
+          0.000651814 * elapsed_years**4 +
+          0.00002373599 * elapsed_years**5
+      when 2_005...2_050
+        elapsed_years = @decimal_year - 2_000
+        62.92 + 0.32217 * elapsed_years + 0.005589 * elapsed_years**2
+      when 2_050...2_150
+        -20 + 32 * ((@decimal_year - 1_820) / 100)**2 - 0.5628 * (2_150 - @decimal_year)
+      when 2_150..3_000
+        -20 + 32 * @decimal_year**2
+      end.round(1)
     end
   end
 end
--- spec/lunation/calculation_spec.rb
+++ spec/lunation/calculation_spec.rb
@@ -99,4 +99,244 @@ RSpec.describe Lunation::Calculation do
 
     it { expect(time).to eq(-0.127296372348) }
   end
+
+  # Reference values for these tests have been taken from: https://eclipse.gsfc.nasa.gov/SEcat5/deltat.html
+  # Date of consulation: 2024-05-19
+  describe "delta_t" do
+    subject(:delta_t) { calculation.delta_t }
+
+    context "when 500 B.C." do
+      let(:datetime) { Date.new(-500, 1, 1) }
+
+      it { expect(delta_t).to eq(17_202.9) } # reference value: 17_203.7
+    end
+
+    context "when 400 B.C." do
+      let(:datetime) { Date.new(-400, 1, 1) }
+
+      it { expect(delta_t).to eq(15_530.3) } # reference value: 15_530.0
+    end
+
+    context "when 300 B.C." do
+      let(:datetime) { Date.new(-300, 1, 1) }
+
+      it { expect(delta_t).to eq(14_077.6) } # reference value: 14_080.0
+    end
+
+    context "when 200 B.C." do
+      let(:datetime) { Date.new(-200, 1, 1) }
+
+      it { expect(delta_t).to eq(12_791.7) } # reference value: 12_790.0
+    end
+
+    context "when 100 B.C." do
+      let(:datetime) { Date.new(-100, 1, 1) }
+
+      it { expect(delta_t).to eq(11_637.1) } # reference value: 11_640.0
+    end
+
+    context "when 0 A.D." do
+      let(:datetime) { Date.new(0, 1, 1) }
+
+      it { expect(delta_t).to eq(10_583.2) } # reference value: 10_580.0
+    end
+
+    context "when 100 A.D." do
+      let(:datetime) { Date.new(100, 1, 1) }
+
+      it { expect(delta_t).to eq(9_596.5) } # reference value: 9_600.0
+    end
+
+    context "when 200 A.D." do
+      let(:datetime) { Date.new(200, 1, 1) }
+
+      it { expect(delta_t).to eq(8_640.3) } # reference value: 8_640.0
+    end
+
+    context "when 300 A.D." do
+      let(:datetime) { Date.new(300, 1, 1) }
+
+      it { expect(delta_t).to eq(7_680.7) } # reference value: 7_680.0
+    end
+
+    context "when 400 A.D." do
+      let(:datetime) { Date.new(400, 1, 1) }
+
+      it { expect(delta_t).to eq(6_698.8) } # reference value: 6_700.0
+    end
+
+    context "when 500 A.D." do
+      let(:datetime) { Date.new(500, 1, 1) }
+
+      it { expect(delta_t).to eq(5_709.6) } # reference value: 5_710.0
+    end
+
+    context "when 600 A.D." do
+      let(:datetime) { Date.new(600, 1, 1) }
+
+      it { expect(delta_t).to eq(4_738.8) } # reference value: 4740.0
+    end
+
+    context "when 700 A.D." do
+      let(:datetime) { Date.new(700, 1, 1) }
+
+      it { expect(delta_t).to eq(3_812.8) } # reference value: 3810.0
+    end
+
+    context "when 800 A.D." do
+      let(:datetime) { Date.new(800, 1, 1) }
+
+      it { expect(delta_t).to eq(2955.4) } # reference value: 2960.0
+    end
+
+    context "when 900 A.D." do
+      let(:datetime) { Date.new(900, 1, 1) }
+
+      it { expect(delta_t).to eq(2200.0) } # reference value: 2200.0
+    end
+
+    context "when 1000 A.D." do
+      let(:datetime) { Date.new(1000, 1, 1) }
+
+      it { expect(delta_t).to eq(1574.0) } # reference value: 1570.0
+    end
+
+    context "when 1100 A.D." do
+      let(:datetime) { Date.new(1100, 1, 1) }
+
+      it { expect(delta_t).to eq(1088.7) } # reference value: 1090.0
+    end
+
+    context "when 1200 A.D." do
+      let(:datetime) { Date.new(1200, 1, 1) }
+
+      it { expect(delta_t).to eq(736.3) } # reference value: 740.0
+    end
+
+    context "when 1300 A.D." do
+      let(:datetime) { Date.new(1300, 1, 1) }
+
+      it { expect(delta_t).to eq(491.8) } # reference value: 490.0
+    end
+
+    context "when 1400 A.D." do
+      let(:datetime) { Date.new(1400, 1, 1) }
+
+      it { expect(delta_t).to eq(321.7) } # reference value: 320.0
+    end
+
+    context "when 1500 A.D." do
+      let(:datetime) { Date.new(1500, 1, 1) }
+
+      it { expect(delta_t).to eq(198.3) } # reference value: 200.0
+    end
+
+    context "when 1600 A.D." do
+      let(:datetime) { Date.new(1_600, 1, 1) }
+
+      it { expect(delta_t).to eq(120.0) } # reference value: 120.0
+    end
+
+    context "when 1700 A.D." do
+      let(:datetime) { Date.new(1_700, 1, 1) }
+
+      it { expect(delta_t).to eq(8.8) } # reference value: 9.0
+    end
+
+    context "when 1750 A.D." do
+      let(:datetime) { Date.new(1_750, 1, 1) }
+
+      it { expect(delta_t).to eq(13.4) } # reference value: 13.0
+    end
+
+    context "when 1800 A.D." do
+      let(:datetime) { Date.new(1_800, 1, 1) }
+
+      it { expect(delta_t).to eq(13.7) } # reference value: 14.0
+    end
+
+    context "when 1850 A.D." do
+      let(:datetime) { Date.new(1_850, 1, 1) }
+
+      it { expect(delta_t).to eq(7.1) } # reference value: 7.0
+    end
+
+    context "when 1900 A.D." do
+      let(:datetime) { Date.new(1_900, 1, 1) }
+
+      it { expect(delta_t).to eq(-2.7) } # reference value: -3.0
+    end
+
+    context "when 1950 A.D." do
+      let(:datetime) { Date.new(1_950, 1, 1) }
+
+      it { expect(delta_t).to eq(29.1) } # reference value: 29.0
+    end
+
+    context "when year 1955 A.D." do
+      let(:datetime) { Date.new(1_955, 1, 1) }
+
+      it { expect(delta_t).to eq(31.1) } # reference value: 31.1
+    end
+
+    context "when year 1960 A.D." do
+      let(:datetime) { Date.new(1_960, 1, 1) }
+
+      it { expect(delta_t).to eq(33.1) } # reference value: 33.2
+    end
+
+    context "when year 1965 A.D." do
+      let(:datetime) { Date.new(1_965, 1, 1) }
+
+      it { expect(delta_t).to eq(35.8) } # reference value: 35.7
+    end
+
+    context "when year 1970 A.D." do
+      let(:datetime) { Date.new(1_970, 1, 1) }
+
+      it { expect(delta_t).to eq(40.2) } # reference value: 40.2
+    end
+
+    context "when year 1975 A.D." do
+      let(:datetime) { Date.new(1_975, 1, 1) }
+
+      it { expect(delta_t).to eq(45.5) } # reference value: 45.5
+    end
+
+    context "when year 1980 A.D." do
+      let(:datetime) { Date.new(1_980, 1, 1) }
+
+      it { expect(delta_t).to eq(50.6) } # reference value: 50.5
+    end
+
+    context "when year 1985 A.D." do
+      let(:datetime) { Date.new(1_985, 1, 1) }
+
+      it { expect(delta_t).to eq(54.4) } # reference value: 54.3
+    end
+
+    context "when year 1990 A.D." do
+      let(:datetime) { Date.new(1_990, 1, 1) }
+
+      it { expect(delta_t).to eq(56.9) } # reference value: 56.9
+    end
+
+    context "when year 1995 A.D." do
+      let(:datetime) { Date.new(1_995, 1, 1) }
+
+      it { expect(delta_t).to eq(60.8) } # reference value: 60.8
+    end
+
+    context "when year 2000 A.D." do
+      let(:datetime) { Date.new(2_000, 1, 1) }
+
+      it { expect(delta_t).to eq(63.9) } # reference value: 63.8
+    end
+
+    context "when year 2005 A.D." do
+      let(:datetime) { Date.new(2_005, 1, 1) }
+
+      it { expect(delta_t).to eq(64.7) } # reference value: 64.7
+    end
+  end
 end
